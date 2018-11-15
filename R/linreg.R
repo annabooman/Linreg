dd2c26c1e5e99ef8e9eb48de9ad75cd3bfb2a80d
Anna Booman <annabooman@Annas-MacBook-Pro-2.local>
2018-11-15 12:07:50 -0500

    First commit

---
+++ R/linreg.R
@@ -0,0 +1,18 @@
+linmodEst <- function(x, y) {
+  coef <- solve(t(x) %*% x) %*% t(x) %*% y
+  ## degrees of freedom and standard deviation of residuals
+  df <- nrow(x) - ncol(x)
+  sigma2 <- sum((y - x %*% coef) ^ 2) / df
+  ## compute sigma^2 * (x’x)^-1
+  vcov <- sigma2 * solve(t(x) %*% x)
+  colnames(vcov) <- rownames(vcov) <- colnames(x)
+  list(
+    coefficients = coef,
+    vcov = vcov,
+    sigma = sqrt(sigma2),
+    df = df
+  )
+}
+
+data(cats, package = "MASS")
+linmodEst(cbind(1, cats$Bwt), cats$Hwt)
